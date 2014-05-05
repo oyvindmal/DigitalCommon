27b74feedac2ad5a12320c46cbb62f1e09e8b200
Øyvind Malin <oyvindmal@outlook.com>
2014-05-05 03:49:18 +0200

    Added /telldus/ prefix to telldus-specific-apis

--- server.js
+++ server.js
@@ -8,16 +8,20 @@ http.createServer(function (req, res) {
 console.log(params);
 //exec("tdtool -f 4", null);
 
-if(params[0] == 'on')
+if(params[0] == 'telldus')
+{
+if(params[1] == 'on')
 {
  res.end("turning on\n");
-exec("tdtool -n " + params[1], null);
+exec("tdtool -n " + params[2], null);
 }
 
-else if(params[0] == 'off')
+else if(params[1] == 'off')
 {
-	res.end("Turning off \n");
-	exec("tdtool -f " + params[1], null);
+        res.end("Turning off \n");
+        exec("tdtool -f " + params[2], null);
+}
+
 }
 else {
   res.end('Invalid Command\n');
