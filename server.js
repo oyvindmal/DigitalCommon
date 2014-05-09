fcc26ad8e69cf1489b221cb83ecac54642709f7e
Øyvind Malin <oyvind@epostboks.org>
2014-05-09 18:42:43 -0400

    Fixed indentation

--- server.js
+++ server.js
@@ -14,35 +14,29 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'application/json'});
  var params = req.url.substring(1).split('/');
 console.log(params);
-//exec("tdtool -f 4", null);
-
 if(params[0] == 'telldus')
 {
-if(params[1] == 'on')
-{
-exec("tdtool -n " + params[2], null);
-obj = { type: 'success', message : 'Turned on switch id ' + params[2], swid: params[2]};
-
-res.end(JSON.stringify(obj));
-}
-
-else if(params[1] == 'off')
-{
+    if(params[1] == 'on')
+    {
+    exec("tdtool -n " + params[2], null);
+        obj = { type: 'success', message : 'Turned on switch id ' + params[2], swid: params[2]};
+        res.end(JSON.stringify(obj));
+    }
+    
+    else if(params[1] == 'off')
+    {
         exec("tdtool -f " + params[2], null);
-	obj = { type: 'success', message : 'Turned off switch id ' + params[2], swid: params[2]};
-	res.end(JSON.stringify(obj));
-
-}
-
-
+        obj = { type: 'success', message : 'Turned off switch id ' + params[2], swid: params[2]};
+        res.end(JSON.stringify(obj));
+    }
+ 
 }
 else {
-obj = { type: 'Error', message : 'invalid'};
-  res.end(JSON.stringify(obj));
+    obj = { type: 'Error', message : 'invalid'};
+    res.end(JSON.stringify(obj));
 
 }
 }).listen(port);
-
 
 console.log('Server running at http://*:' + port);
 
