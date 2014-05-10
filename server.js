f2b788a4484de8e1e37a2e24b671f5cc683cdd48
Øyvind Malin <oyvind@epostboks.org>
2014-05-10 11:26:27 -0400

    Added first event

--- server.js
+++ server.js
@@ -24,15 +24,15 @@ function telldusOff(swid)
 // Web API
 
 http.createServer(function (req, res) {
-    
+
     res.writeHead(200, {'Content-Type': 'application/json'});
-    
+
     var params = req.url.substring(1).split('/');
-    
+
     console.log(params);
-    
+
     var controllerName = params[0];
-    
+
         if(controllerName == 'telldus')
         {
             var command = params[1];
@@ -41,36 +41,36 @@ http.createServer(function (req, res) {
             {
                 res.end(JSON.stringify(telldusOn(swid)));
             }
-            
+
             if(command == 'off')
             {
                 res.end(JSON.stringify(telldusOff(swid)));
             }
-         
+
         }
         else {
             var obj = { type: 'Error', message : 'invalid'};
             res.end(JSON.stringify(obj));
-        
+
         }
 }).listen(webApiPort);
 
 console.log('Server running at http://*:' + webApiPort);
 
 //Telldus event listener
- 
+
 // Set up a connection to the TelldusEvents socket
 var conn = net.createConnection('/tmp/TelldusEvents');
- 
+
 // Set the encoding so that you get data that is actually readable by humans
 conn.setEncoding('utf-8');
- 
+
 //Eventlistener on connection
 conn.on('connect' , function () {
         console.log("Connected");
 });
- 
- 
+
+
 //Eventlistener on data recieved
 conn.on('data', function(data) {
         //Log the data to the console
