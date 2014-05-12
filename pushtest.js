ca1410e9aa5c8ba646f4d366aebfccbd4bf783a3
Øyvind Malin <oyvind@epostboks.org>
2014-05-11 20:41:10 -0400

    added function for sending push

--- pushtest.js
+++ pushtest.js
@@ -1,3 +1,39 @@
+var https = require("https");
+var fs  = require("fs");
+sendMessage("lol");
+function sendMessage(message)
+{
+var obj = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+var secret = obj.pushcoSecret;
+var key = obj.pushcoKey;
+var hostname = 'api.push.co';
+var base = "/1.0/push/";
 
-var file = require('./config.json');
-console.log(JSON.stringify(file));
+var url = base + "?message=" + message + "&api_key=" + key + "&api_secret=" + secret;
+var options = {
+    host: hostname,
+    port: 443,
+    path: url,
+    method: 'POST'
+};
+
+var req = https.request(options, function(res){
+    console.log('status: ' + res.statusCode);
+    console.log('headers: ' + JSON.stringify(res.headers));
+    res.setEncoding('utf8');
+    res.on('data', function(chunk){
+        console.log("body: " + chunk);
+    });
+});
+
+req.on('error', function(e) {
+    console.log('problem with request: ' + e.message);
+    console.log(e);
+});
+
+// write data to request body
+req.write('data\n');
+req.write('data\n');
+req.end();
+console.log(url);    
+}
