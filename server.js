3938e9361480585a02242181431d34c7edd3f741
Øyvind Malin <oyvind@epostboks.org>
2014-05-11 20:49:43 -0400

    first attempt at making pushnotifications from server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 //Requirements
 var net = require('net');
+var https = require("https");
+var fs  = require("fs");
 var http = require('http');
 var sys = require('sys');
 var exec = require('child_process').exec;
@@ -19,6 +21,57 @@ function telldusOff(swid)
 {
     exec("tdtool -f " + swid, null);
     return { type: 'success', message : 'Turned off switch id ' + swid, swid: swid};
+}
+
+//pushco
+function sendMessage(message)
+{
+var obj = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+var secret = obj.pushcoSecret;
+var key = obj.pushcoKey;
+var hostname = 'api.push.co';
+var base = "/1.0/push/";
+
+var jsonobj = {
+    "message" : message,
+    "api_key" : key,
+    "api_secret" : secret
+}
+console.log(jsonobj)
+
+var jsonobjString = JSON.stringify(jsonobj);
+var headers = {
+    'Content-Type:' : 'application/json',
+    'Content-Length' : jsonobjString.length
+}
+var options = {
+    host: hostname,
+    port: 443,
+    path: base,
+    method: 'POST',
+    headers: headers
+};
+
+
+
+var req = https.request(options, function(res){
+    console.log('status: ' + res.statusCode);
+    console.log('headers: ' + JSON.stringify(res.headers));
+    res.setEncoding('utf8');
+    res.on('data', function(chunk){
+        console.log("body: " + chunk);
+    });
+});
+
+req.on('error', function(e) {
+    console.log('problem with request: ' + e.message);
+    console.log(e);
+});
+
+// write data to request body
+req.write(jsonobjString)
+req.end();
+console.log(base);    
 }
 
 // Web API
@@ -80,11 +133,13 @@ conn.on('data', function(data) {
        {
            console.log("Switch OFF 1");
            telldusOff(4);
+           sendMessage("Turning off main lights");
        }
         if(data == '16:TDRawDeviceEvent95:class:command;protocol:arctech;model:selflearning;house:11667822;unit:12;group:0;method:turnon;i1s')
        {
            console.log("Switch ON 1");
            telldusOn(4);
+           sendMessage("Turning on main lights");
        }
        if(data == '16:TDRawDeviceEvent95:class:command;protocol:arctech;model:selflearning;house:11667822;unit:11;group:0;method:turnon;i1s')
        {
