58404f9061834cd73bbe3f90a8d3499b06151f5c
Øyvind Malin <oyvind@epostboks.org>
2014-05-18 20:37:41 -0400

    Removed old obsolete content from server.js

--- server.js
+++ server.js
@@ -1,67 +1,11 @@
 //Requirements
 var net = require('net');
-var https = require("https");
-var fs  = require("fs");
 var http = require('http');
-var sys = require('sys');
 var telldus = require("./lib/controllers/telldus.js");
 var parser = require("./lib/parser.js");
 
 //Configurations
 var webApiPort = 8081;
-
-
-
-//pushco
-function sendMessage(message)
-{
-var obj = JSON.parse(fs.readFileSync('config.json', 'utf8'));
-var secret = obj.pushcoSecret;
-var key = obj.pushcoKey;
-var hostname = 'api.push.co';
-var base = "/1.0/push/";
-
-var jsonobj = {
-    "message" : message,
-    "api_key" : key,
-    "api_secret" : secret
-}
-
-
-var jsonobjString = JSON.stringify(jsonobj);
-var headers = {
-    'Content-Type:' : 'application/json',
-    'Content-Length' : jsonobjString.length
-}
-var options = {
-    host: hostname,
-    port: 443,
-    path: base,
-    method: 'POST',
-    headers: headers
-};
-
-
-
-var req = https.request(options, function(res){
-    console.log('status: ' + res.statusCode);
-    console.log('headers: ' + JSON.stringify(res.headers));
-    res.setEncoding('utf8');
-    res.on('data', function(chunk){
-        console.log("body: " + chunk);
-    });
-});
-
-req.on('error', function(e) {
-    console.log('problem with request: ' + e.message);
-    console.log(e);
-});
-
-// write data to request body
-req.write(jsonobjString)
-req.end();
-   
-}
 
 // Web API
 
